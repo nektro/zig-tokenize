1b332a15675ef0274b8b5dba5a4c3be621dedc5c
Meghan <hello@nektro.net>
2023-02-27 20:23:34 -0800

    Create example.js

---
+++ example.js
@@ -0,0 +1,6 @@
+let x, y, z;  // Declare 3 variables
+x = 5;        // Assign the value 5 to x
+y = 6;        // Assign the value 6 to y
+z = x + y;    // Assign the sum of x and y to z
+
+document.getElementById("demo").innerHTML = "The value of z is " + z + ".";
